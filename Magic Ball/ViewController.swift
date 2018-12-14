615cf36ea06b5d1a712c6db8648eddf72398f04d
LinaChasova <a.chasova@innopolis.ru>
2018-12-14 10:37:40 +0300

    change ball image on button press

--- Magic Ball/ViewController.swift
+++ Magic Ball/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImage: UIImageView!
+    var ballImageNumber : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        displayImage()
+        
+    }
+    
+    func displayImage() {
+        ballImageNumber = Int(arc4random_uniform(5) + 1)
+        
+        ballImage.image = UIImage(named: "ball\(ballImageNumber)")
+    }
+    
 }
 
